435033aea35c7fdb52062403183593e3aa45b64f
mika.arip <mika.arip@quadrant-si.id>
2025-08-31 16:30:38 +0700

    update the otp type data

--- app/Notifications/BrevoOTPNotification.php
+++ app/Notifications/BrevoOTPNotification.php
@@ -9,34 +9,37 @@ class BrevoOTPNotification extends Notification
 {
     use Queueable;
 
-    protected $token;
+    protected $otp;
 
-    public function __construct(string $token)
+    public function __construct(array $otp)
     {
-        $this->token = $token;
+        $this->otp = $otp;
     }
 
     public function via($notifiable)
     {
-        return ['brevo']; // custom channel
+        return ['brevo'];
     }
 
     public function toBrevo($notifiable)
     {
+        $code = $this->otp['token'];
+
         return Http::withHeaders([
             'api-key' => env('BREVO_API_KEY'),
             'accept' => 'application/json',
             'content-type' => 'application/json',
         ])->post('https://api.brevo.com/v3/smtp/email', [
             'sender' => [
-                'name' => config('mail.from.name'),
+                'name'  => config('mail.from.name'),
                 'email' => config('mail.from.address'),
             ],
             'to' => [
                 ['email' => $notifiable->email],
             ],
             'subject' => 'Your OTP Code',
-            'htmlContent' => "<p>Your OTP is <strong>{$this->token}</strong></p>",
+            'htmlContent' => "<p>Your OTP is <strong>{$code}</strong></p>",
         ]);
     }
 }
+
